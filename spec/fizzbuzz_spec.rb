a48956a1cf7a2e83a14ae0107e070f085bc21805
Nigel Said <nigelsaid@gmail.com>
2017-04-03 16:18:04 +0100

    Adds third test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,5 +8,9 @@ describe "Fizzbuzz" do
   it "returns 'buzz' when passed 5" do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
-  
+
+  it "returns 'fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
 end
