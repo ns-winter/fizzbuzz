11e61d397e88e9eedff41c982f5663d0ad69d145
Nigel Said <nigelsaid@gmail.com>
2017-04-03 16:29:05 +0100

    Adds seventh test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -24,5 +24,8 @@ describe "Fizzbuzz" do
   it 'returns "buzz" when passed 10' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end
-  
+
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
 end
